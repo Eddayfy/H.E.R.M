56bac80ed1f7b00bb69185ff72d951731516552d
Eddayfy <46305054+Eddayfy@users.noreply.github.com>
2020-04-09 18:56:19 +0100

    H.E.R.M

---
+++ Facture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H.E.R.M_App22
+{
+    class Facture
+    {
+        private int Id;
+        private string nom_complet;
+        private DateTime Date;
+        private int telephone;
+
+        public int id
+        {
+            get => Id;
+            set => Id = value;
+        }
+        public string Nom_complet
+        {
+            get => nom_complet;
+            set => nom_complet = value;
+        }
+        public DateTime date
+        {
+            get => date;
+            set => date = value;
+        }
+
+        public int Telephone
+        {
+            get => telephone;
+            set => telephone = value;
+        }
+
+        public Facture(string nom_complet,DateTime date,int telephone)
+        {
+            this.Id = id;
+            this.nom_complet = nom_complet;
+            this.Date = date;
+            this.telephone = telephone;
+        } 
+
+    }
+}
---
+++ Matiere premier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H.E.R.M_App22
+{
+    class Matiere_premier
+    {
+        private int Id;
+        private string libell;
+        private double Quntite;
+
+        public int id
+        {
+            get => id;
+            set => id = value;
+        }
+        public string Libell
+        {
+            get => libell;
+            set => libell = value;
+        }
+        public double quntite
+        {
+            get => Quntite;
+            set => Quntite = value;
+        }
+        public Matiere_premier(int Id,string libell,double Quntite)
+        {
+            this.Id = id;
+            this.libell = Libell;
+            this.Quntite = quntite;
+        }
+    }
+}
